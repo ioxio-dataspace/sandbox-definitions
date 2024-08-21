20a7c2a8997160dfc233492a76c0707c34e2b3a6
Andrew <dyusha.ru@gmail.com>
2024-08-21 22:05:48 +0800

    description updated, dots added

--- src/DigitalProductPassport/EnvironmentalFootprint_v0.1.py
+++ src/DigitalProductPassport/EnvironmentalFootprint_v0.1.py
@@ -9,28 +9,28 @@ class CarbonFootprint(CamelCaseModel):
         None,
         title="Raw material footprint (kg of CO2e)",
         description="The carbon footprint of the raw material acquisition "
-        "of the product calculated as kg of CO2e using PEF and PEFCR methods",
+        "of the product calculated as kg of CO2e using PEF and PEFCR methods.",
         examples=[1546.3],
     )
     component_production_footprint: Optional[float] = Field(
         None,
         title="Component production footprint (kg of CO2e)",
         description="The carbon footprint of the component production phase of the product "
-        "calculated as kg of CO2e using PEF and PEFCR methods",
+        "calculated as kg of CO2e using PEF and PEFCR methods.",
         examples=[2345.7],
     )
     main_production_footprint: Optional[float] = Field(
         None,
         title="Main production footprint (kg of CO2e)",
         description="The carbon footprint of the main production or assembly of "
-        "the product calculated as kg of CO2e using PEF and PEFCR methods",
+        "the product calculated as kg of CO2e using PEF and PEFCR methods.",
         examples=[3504.4],
     )
     transportation_footprint: Optional[float] = Field(
         None,
         title="Transportation footprint (kg of CO2e)",
         description="The carbon footprint of the component transportation calculated "
-        "as kg of CO2e using PEF and PEFCR methods",
+        "as kg of CO2e using PEF and PEFCR methods.",
         examples=[2780.3],
     )
     reference_material: Optional[str] = Field(
@@ -38,7 +38,7 @@ class CarbonFootprint(CamelCaseModel):
         pattern=r"^https://",
         max_length=2083,
         title="Reference material",
-        description="The link to the study supporting the carbon footprint values",
+        description="The link to the study supporting the carbon footprint values.",
         examples=["https://example.com/CarbonFootprint"],
     )
 
@@ -47,7 +47,7 @@ class MaterialWaste(CamelCaseModel):
     amount: Optional[float] = Field(
         None,
         title="Amount (kg)",
-        description="The amount of material waste in kilograms generated during the production",
+        description="The amount of material waste in kilograms generated during the production.",
         examples=[500],
     )
     reference_material: Optional[str] = Field(
@@ -55,7 +55,7 @@ class MaterialWaste(CamelCaseModel):
         pattern=r"^https://",
         max_length=2083,
         title="Reference material",
-        description="The link to the study supporting the material waste values",
+        description="The link to the study supporting the material waste values.",
         examples=["https://example.com/materialWaste"],
     )
 
@@ -64,12 +64,12 @@ class ProductEnvironmentalFootprintResponse(CamelCaseModel):
     carbon_footprint: CarbonFootprint = Field(
         ...,
         title="Carbon footprint",
-        description="The details of the carbon footprint during production",
+        description="The details of the carbon footprint during production.",
     )
     material_waste: Optional[MaterialWaste] = Field(
         None,
         title="Material waste",
-        description="The details of the material waste generated during production",
+        description="The details of the material waste generated during production.",
     )
 
 
@@ -78,14 +78,14 @@ class ProductEnvironmentalFootprintRequest(CamelCaseModel):
         ...,
         max_length=150,
         title="Product",
-        description="The product code used for identifying the product type",
+        description="The product code used for identifying the product type.",
         examples=["product-modelX-1234"],
     )
     id: str = Field(
         ...,
         max_length=40,
         title="ID",
-        description="The unique identifier of the product",
+        description="The unique identifier of the product.",
         examples=["71b51878-8a00-11ee-b9d1-0242ac120002"],
     )
 
@@ -93,7 +93,7 @@ class ProductEnvironmentalFootprintRequest(CamelCaseModel):
 DEFINITION = DataProductDefinition(
     version="0.1.0",
     title="Product environmental footprint",
-    description="Carbon footprint of a product",
+    description="The environmental impact of the product manufacturing",
     request=ProductEnvironmentalFootprintRequest,
     response=ProductEnvironmentalFootprintResponse,
     tags=[
