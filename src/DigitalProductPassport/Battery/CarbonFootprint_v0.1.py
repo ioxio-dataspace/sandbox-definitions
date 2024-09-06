a1596f7f8f7aa49fe37a19895d33791460e9219b
Joakim Nordling <joakim.nordling@ioxio.com>
2024-09-06 13:08:08 +0300

    Updates to Battery Carbon Footprint
    Add dots to end of descriptions

--- src/DigitalProductPassport/Battery/CarbonFootprint_v0.1.py
+++ src/DigitalProductPassport/Battery/CarbonFootprint_v0.1.py
@@ -10,14 +10,14 @@ class ManufacturingLocation(CamelCaseModel):
         title="Country",
         pattern=r"^[A-Z]{3}$",
         description="The country code of the battery manufacturing location in Alpha-3 "
-        "format",
+        "format.",
         examples=["DEU"],
     )
     city: Optional[str] = Field(
         None,
         title="City",
         max_length=40,
-        description="The city of the battery manufacturing location",
+        description="The city of the battery manufacturing location.",
         examples=["Hamburg"],
     )
 
@@ -27,28 +27,28 @@ class ManufacturerInformation(CamelCaseModel):
         None,
         max_length=250,
         title="Name",
-        description="The registered trade name of the manufacturer company",
+        description="The registered trade name of the manufacturer company.",
         examples=["Battery Manufacturer A"],
     )
     street_name: Optional[str] = Field(
         None,
         title="Street Name",
         max_length=40,
-        description="The street address of the manufacturer's headquarters",
+        description="The street address of the manufacturer's headquarters.",
         examples=["Example Street 100"],
     )
     postal_code: Optional[str] = Field(
         None,
         title="Postal Code",
         max_length=10,
-        description="The postal code of the manufacturer's headquarters",
+        description="The postal code of the manufacturer's headquarters.",
         examples=["75034"],
     )
     city: Optional[str] = Field(
         None,
         title="City",
         max_length=40,
-        description="The city of the manufacturer's headquarters",
+        description="The city of the manufacturer's headquarters.",
         examples=["Seattle"],
     )
     country: Optional[str] = Field(
@@ -56,7 +56,7 @@ class ManufacturerInformation(CamelCaseModel):
         title="Country",
         pattern=r"^[A-Z]{3}$",
         description="The country code of the manufacturer's headquarters location in "
-        "Alpha-3 format",
+        "Alpha-3 format.",
         examples=["USA"],
     )
     website: Optional[str] = Field(
@@ -64,13 +64,13 @@ class ManufacturerInformation(CamelCaseModel):
         pattern=r"^https://",
         max_length=2083,
         title="Website",
-        description="The website of the manufacturer",
+        description="The website of the manufacturer.",
         examples=["https://example.com/"],
     )
     email: Optional[EmailStr] = Field(
         None,
         title="Email",
-        description="The email address of the manufacturer",
+        description="The email address of the manufacturer.",
         examples=["info@example.com"],
     )
 
@@ -81,7 +81,7 @@ class CarbonFootprint(CamelCaseModel):
         title="Pre Production Footprint",
         description="The carbon footprint of the raw material acquisition and "
         "pre-processing phase of the battery calculated as kilograms (kg) of CO2e per "
-        "one kilowatt-hour (kWh) using preferably PEF and PEFCR methods",
+        "one kilowatt-hour (kWh) using preferably PEF and PEFCR methods.",
         examples=[2345.7],
     )
     main_production_footprint: Optional[float] = Field(
@@ -89,7 +89,7 @@ class CarbonFootprint(CamelCaseModel):
         title="Main Production Footprint",
         description="The carbon footprint of the battery main production phase "
         "calculated as kilograms (kg) of CO2e per one kilowatt-hour (kWh) using "
-        "preferably PEF and PEFCR methods",
+        "preferably PEF and PEFCR methods.",
         examples=[3504.4],
     )
     reference_material: Optional[str] = Field(
@@ -98,7 +98,7 @@ class CarbonFootprint(CamelCaseModel):
         max_length=2083,
         title="Reference Material",
         description="The web link giving access to a public version of the study "
-        "supporting the carbon footprint values",
+        "supporting the carbon footprint values.",
         examples=["https://example.com/CarbonFootprint"],
     )
 
@@ -107,13 +107,13 @@ class CarbonFootprintResponse(CamelCaseModel):
     manufacturer_information: Optional[ManufacturerInformation] = Field(
         None,
         title="Manufacturer Information",
-        description="The details of the battery manufacturer",
+        description="The details of the battery manufacturer.",
     )
     battery_model: Optional[str] = Field(
         None,
         max_length=40,
         title="Battery Model",
-        description="The model of the battery",
+        description="The model of the battery.",
         examples=["Z37-310-76"],
     )
     conformity_declaration: Optional[str] = Field(
@@ -121,19 +121,19 @@ class CarbonFootprintResponse(CamelCaseModel):
         pattern=r"^https://",
         max_length=2083,
         title="Conformity Declaration",
-        description="The link to the EU declaration of conformity documentation",
+        description="The link to the EU declaration of conformity documentation.",
         examples=["https://example.com/EUdeclaration"],
     )
     manufacturing_location: Optional[ManufacturingLocation] = Field(
         None,
         title="Manufacturing Location",
-        description="The details of the location of the battery manufacturing plant",
+        description="The details of the location of the battery manufacturing plant.",
     )
     carbon_footprint: Optional[CarbonFootprint] = Field(
         None,
         title="Carbon Footprint",
         description="The details of the carbon footprint for the battery production "
-        "phases",
+        "phases.",
     )
 
 
@@ -142,23 +142,23 @@ class CarbonFootprintRequest(CamelCaseModel):
         ...,
         max_length=150,
         title="Product",
-        description="The product code used for identifying the product type",
+        description="The product code used for identifying the product type.",
         examples=["sodium-ion-75kWh"],
     )
     id: str = Field(
         ...,
         max_length=40,
         title="Id",
-        description="The unique identifier of the product",
+        description="The unique identifier of the product.",
         examples=["660e8400-e29b-41d4-a716-446655440000"],
     )
 
 
 DEFINITION = DataProductDefinition(
-    version="0.1.1",
+    version="0.1.2",
     title="Battery Carbon Footprint",
     description="Carbon footprint of a battery as required by the European "
-    "Commission's Battery Act (2023/1542)",
+    "Commission's Battery Act (2023/1542).",
     request=CarbonFootprintRequest,
     response=CarbonFootprintResponse,
 )
