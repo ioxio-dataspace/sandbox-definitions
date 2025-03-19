38a5b6b68682da859bbd432d8a4694357224bc90
Pekka <pekka@cocreators.ee>
2025-03-19 11:53:19 +0200

    punctuation and tags

--- src/Weather/Current/Metric_v1.0.py
+++ src/Weather/Current/Metric_v1.0.py
@@ -6,7 +6,7 @@ class CurrentWeatherMetricRequest(CamelCaseModel):
     lat: float = Field(
         ...,
         title="Latitude",
-        description="The latitude coordinate of the desired location",
+        description="The latitude coordinate of the desired location.",
         ge=-90.0,
         le=90.0,
         examples=[60.192059],
@@ -14,7 +14,7 @@ class CurrentWeatherMetricRequest(CamelCaseModel):
     lon: float = Field(
         ...,
         title="Longitude",
-        description="The longitude coordinate of the desired location",
+        description="The longitude coordinate of the desired location.",
         ge=-180.0,
         le=180.0,
         examples=[24.945831],
@@ -30,10 +30,10 @@ class CurrentWeatherMetricResponse(CamelCaseModel):
         ..., title="Rain status", description="If it's currently raining or not."
     )
     temp: float = Field(
-        ..., title="Current temperature in Celsius", examples=[17.3], ge=-273.15
+        ..., title="Current temperature in Celsius.", examples=[17.3], ge=-273.15
     )
     wind_speed: float = Field(
-        ..., title="Current wind speed in m/s", examples=[2.1], ge=0
+        ..., title="Current wind speed in m/s.", examples=[2.1], ge=0
     )
     wind_direction: float = Field(
         ...,
